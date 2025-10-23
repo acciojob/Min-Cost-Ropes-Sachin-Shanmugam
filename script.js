4c3635373077fd619bd1058f5259d9a9d325063c
Sachin-Shanmugam <sachinshanmugam19@gmail.com>
2025-10-24 00:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+const heap = require('heap');
+const minHeap = new heap.MinHeap();
+	for (let length of arr) {
+		minHeap.push(length);
+	}
+let totalcost = 0;
+  while (minHeap.size() > 1) {
+  	let first = minHeap.pop();
+	  let second = minHeap.pop();
+
+	let cost = first + second ;
+	  let totalcost += cost;
+
+	  minHeap.push(cost);
+  }
+	return totalcost;
 }
 
 module.exports=mincost;
